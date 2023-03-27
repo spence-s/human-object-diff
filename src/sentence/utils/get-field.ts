c5ed74131f01d87b80027e07d36eb46c0796f7e4
Spencer Snyder <sasnyde2@gmail.com>
2023-03-27 17:11:08 -0400

    chore: lint and format

--- src/sentence/utils/get-field.ts
+++ src/sentence/utils/get-field.ts
@@ -1,20 +1,20 @@
-import { type DiffContext } from '../index';
-import humanize from './humanize';
+import { type DiffContext } from '../index'
+import humanize from './humanize'
 
 export default function getField(context: DiffContext): string {
   if (typeof context.diff === 'string') {
-    return '';
+    return ''
   }
 
   if (context.diff.path) {
-    let propertyIndex = context.diff.path.length - 1;
+    let propertyIndex = context.diff.path.length - 1
     while (typeof context.diff.path[propertyIndex] !== 'string') {
-      propertyIndex -= 1;
+      propertyIndex -= 1
     }
 
-    const property = context.diff.path[propertyIndex];
-    return humanize(String(property), context.config);
+    const property = context.diff.path[propertyIndex]
+    return humanize(String(property), context.config)
   }
 
-  return '';
+  return ''
 }
--- test/ignore-arrays.test.ts
+++ test/ignore-arrays.test.ts
@@ -1,15 +1,15 @@
-import HR from '../src/index';
+import HR from '../src/index'
 
 describe('ignore-arrays', () => {
-  let hr: Function;
+  let hr: Function
 
   beforeEach(() => {
-    hr = new HR({ ignoreArrays: true }).diff;
-  });
+    hr = new HR({ ignoreArrays: true }).diff
+  })
 
   test('humanReadable is a function', () => {
-    expect(typeof hr === 'function').toBeTruthy();
-  });
+    expect(typeof hr === 'function').toBeTruthy()
+  })
 
   test('ignores arrays', () => {
     const lhs = {
@@ -18,21 +18,21 @@ describe('ignore-arrays', () => {
       baz: 12,
       chip: 'dale',
       biz: { baz: [1, 2, { hello: ['then'] }, 4, 5] },
-      base: [1, 2, 3, 4, 5]
-    };
+      base: [1, 2, 3, 4, 5],
+    }
 
     const rhs = {
       foo: [1, 2, 3, 4, 5, 5, 7, 8, 9, 10, 11],
       bar: 'hello world',
       baz: 10,
       biz: { baz: [1, 2, { hello: ['there', 'was'] }, 5] },
-      base: [1, 2, 2, 5]
-    };
+      base: [1, 2, 2, 5],
+    }
 
     expect(hr(lhs, rhs)).toEqual([
       '"Bar", with a value of "hello" (at Obj.bar) was changed to "hello world"',
       '"Baz", with a value of "12" (at Obj.baz) was changed to "10"',
-      '"Chip", with a value of "dale" (at Obj.chip) was removed'
-    ]);
-  });
-});
+      '"Chip", with a value of "dale" (at Obj.chip) was removed',
+    ])
+  })
+})
